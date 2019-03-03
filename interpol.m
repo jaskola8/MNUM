daa1d340994e1d6358283759a325f92439baabb7
jstarczewski <jan.a.starczewski@gmail.com>
2019-03-03 22:55:31 +0100

    hotfixes

--- interpol.m
+++ interpol.m
@@ -9,7 +9,7 @@ xs = zeros(3, 1000);
 xs(1,:) = linspace(x1, xn, 1000);
 for i = 1:1000
     xs(2,i) = func(xs(1,i));
-    xs(3,i) = legrange(data, xs(1,i));
+    xs(3,i) = newton(data, xs(1,i));
 end
 
 plot(xs(1,:),xs(2,:),xs(1,:),xs(3,:), "r--")
--- newton.m
+++ newton.m
@@ -1,7 +1,7 @@
 function func = newton(data, x)
     func = 0.0;
     u = data(1,:);
-    r = zeros(4);
+    r = zeros(size(u,2));
     r(:,1) = data(2,:);
     for j = 1:1:size(u,2)
         z = size(u,2);
@@ -16,11 +16,10 @@ function func = newton(data, x)
     for i = 1:1:size(u,2)
         base = 1;
         for j = 1:1:i-1
-            disp("i " + i + " j " + j + " u(j) "+ u(j));
             base = base * (x-u(j));      
         end
         func = func + r(1,i)*base;
     end
-    disp(r)
+    %disp(r)
             
     
