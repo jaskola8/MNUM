515c7bdae26fe3ee294ac12dcf24f13035abd85a
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-05-26 13:23:19 +0200

    Euler and heun methods implemented

---
+++ Roz/ueuler.m
@@ -0,0 +1,15 @@
+function ueuler(f, ft, h, y0, x0, t0, min, max)
+    w_count = length(min:h:max);
+    w = zeros(3,w_count);
+    w(:,1) = [x0, y0, t0];
+    for i = 2:w_count
+        dx = w(1,i-1);
+        dy = w(2, i-1);
+        dt = w(3, i-1);
+        xn = dx + h*f(dx, dy, dt);
+        yn = dy + h*ft(dx, dy, dt);
+        tn = dt + h;
+        w(:,i) = [xn,yn,tn];
+    end
+    disp(w);
+end
---
+++ Roz/uheun.m
@@ -0,0 +1,15 @@
+function uheun(f, ft, h, y0, x0, t0, min, max)
+    w_count = length(min:h:max);
+    w = zeros(3,w_count);
+    w(:,1) = [x0, y0, t0];
+     for i = 2:w_count
+        dx = w(1,i-1);
+        dy = w(2, i-1);
+        dt = w(3, i-1);
+        xn = dx + h*(0.5*(f(dx,dy,dt)+f(dx+h, dy+h*f(dx,dy,dt))));
+        yn = dy + h*(0.5*(ft(dx,dy,dt)+ft(dx+h, dy+h*ft(dx,dy,dt))));
+        tn = dt + h;
+        w(:,i) = [xn, yn, tn];
+     end 
+        disp(w);
+end
